ea3bc1e8aa9e072bf93b131816e0d20e1795c999
Bernat Brunet Torruella <bernat@nan-tic.com>
2013-05-02 13:09:53 +0200

    Add new filed in the user profile, used for codereview to send mail from all uploads and comments

--- examples/rietveld/rietveld_helper/admin.py
+++ examples/rietveld/rietveld_helper/admin.py
@@ -1,6 +1,27 @@
 from django.contrib import admin
-
 from codereview import models
+
+
+from django.contrib.auth.admin import UserAdmin as OriginalUserAdmin
+from django.contrib.auth.models import User
+from rietveld_helper.models import CodereviewProfile
+
+
+class CodereviewProfileInline(admin.StackedInline):
+    """ As you are noticed your profile will be edited as inline form """
+    model = CodereviewProfile
+    can_delete = False
+
+
+class UserAdmin(OriginalUserAdmin):
+    """ Just add inlines to the original UserAdmin class """
+    inlines = [CodereviewProfileInline, ]
+
+try:
+    admin.site.unregister(User)
+finally:
+    admin.site.register(User, UserAdmin)
+
 
 # Patch in some simple lambda's, Django uses them.
 #models.Issue.__unicode__ = lambda self: self.subject
@@ -15,6 +36,7 @@ from codereview import models
 #    list_filter = ('issue', 'owner')
 #    list_display = ('issue', 'message')
 #    search_fields = ('issue__subject', 'message')
+
 
 #class IssueAdmin(admin.ModelAdmin):
 #    list_filter = ('closed', 'owner')
--- examples/rietveld/rietveld_helper/models.py
+++ examples/rietveld/rietveld_helper/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class CodereviewProfile(models.Model):
+    # Link to the user model
+    user = models.OneToOneField(User, related_name='profile', unique=False)
+    main_reviewer = models.BooleanField(u"Main Reviewer", default=False,
+                                        blank=True, help_text="Designates that\
+                                        this user receive all mails from\
+                                        codereviews uploads and comments")
+
+    def __str__(self):
+        return "%s's profile" % self.user
+
 
 # Create your models here.
